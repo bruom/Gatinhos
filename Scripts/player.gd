f752ae13062251885aff6ea3980e5391cff19925
Omella <bruno.omella@hotmail.com>
2023-06-24 19:03:36 -0300

    Creating a basic shader for testing

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,11 @@
 extends CharacterBody3D
 
-@onready var sound_debug: MeshInstance3D = $sound_debug
-
 @export var speed: float = 1.0
 @export var sound_radius: float = 1.5
 var current_sound_radius: float = 0.0
 
 func _process(delta):
+	RenderingServer.global_shader_parameter_set("player_position", global_position)
 	var direciton: Vector2 = Input.get_vector("Left", "Right", "Forward", "Backward")
 	velocity = Vector3(direciton.x, 0.0, direciton.y).normalized() * speed
 	if velocity != Vector3.ZERO:
@@ -15,5 +14,3 @@ func _process(delta):
 		move_and_slide()
 	else:
 		current_sound_radius = 0.0
-	
-	sound_debug.mesh.bottom_radius = current_sound_radius
