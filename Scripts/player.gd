a9ec67b3321dca5a0107dc97a8b9bbf69cbc30c2
Omella <bruno.omella@hotmail.com>
2023-06-24 17:01:27 -0300

    Enemy patrol and chase behavior

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,4 +5,6 @@ extends CharacterBody3D
 func _process(delta):
 	var direciton: Vector2 = Input.get_vector("Left", "Right", "Forward", "Backward")
 	velocity = Vector3(direciton.x, 0.0, direciton.y).normalized() * speed
-	move_and_slide()
+	if velocity != Vector3.ZERO:
+		look_at(global_position + velocity)
+		move_and_slide()
