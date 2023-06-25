8d94a60c2775d1c233348a30edb87bf2e51f3ca6
Vitor Kawai Sala <vitorsala@gmail.com>
2023-06-25 18:40:38 -0300

    Noise emmiter fix

--- Scripts/noiseEmitter.gd
+++ Scripts/noiseEmitter.gd
@@ -5,29 +5,34 @@ signal noise_emitted(origin: Vector3, strength: float)
 @export var noise_audio_stream: AudioStream
 @onready var audio_player = $AudioStreamPlayer3D
 
-#@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
-func emit_noise(strenght: float = 1):
-	noise_emitted.emit(global_position, strenght)
+func emit_noise(strength: float = 1):
+	noise_emitted.emit(global_position, strength)
 	play_sound()
 	
-#	var shape = CylinderMesh.new()
-#	var material = StandardMaterial3D.new()
-#	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
-#	material.albedo_color = Color(0, 1, 0, 0.5)
-#	shape.height = 0.1
-#	shape.top_radius = strenght
-#	shape.bottom_radius = strenght
-#	shape.material = material
-#	mesh_instance_3d.mesh = shape
-#	mesh_instance_3d.show()
-#	var timer = Timer.new()
-#	timer.one_shot = true
-#	timer.timeout.connect(func():
-#		mesh_instance_3d.hide()
-#	)
-#	add_child(timer)
-#	timer.start(0.2)
+	create_debug_range(strength)
+
+func create_debug_range(strength: float):
+	var mesh_instance_3d: MeshInstance3D = MeshInstance3D.new()
+	var shape = CylinderMesh.new()
+	var material = StandardMaterial3D.new()
+	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	material.albedo_color = Color(0, 1, 0, 0.5)
+	shape.height = 0.1
+	shape.top_radius = strength
+	shape.bottom_radius = strength
+	shape.material = material
+	mesh_instance_3d.mesh = shape
+	var timer = Timer.new()
+	timer.one_shot = true
+	
+	add_child(mesh_instance_3d)
+	timer.timeout.connect(func():
+		mesh_instance_3d.queue_free()
+	)
+	add_child(timer)
+	timer.start(0.2)
 	
 func play_sound():
 	if !noise_audio_stream: return
