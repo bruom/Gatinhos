cf4587c54a29968a87a17e09ed618a0d240352c6
Vitor Kawai Sala <1914295+vkawai@users.noreply.github.com>
2023-07-02 18:01:53 -0300

    Making configuration able to change audio BUS's volume

--- Scripts/UI/Menu/configuration_slider_entry.gd
+++ Scripts/UI/Menu/configuration_slider_entry.gd
@@ -7,7 +7,9 @@ signal value_changed(float)
 @onready var label: Label = $Label
 @onready var slider: Slider = $slider
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	slider.value_changed.connect(func(value): value_changed.emit(value_changed))
+	slider.value_changed.connect(func(value): value_changed.emit(value))
 	label.text = tr(label_content)
+
+func set_slider_value(value: float):
+	slider.value = value
